865c11cd3fe798cc5614ebb651f129cac2497d7f
sky-go212 <gxuyaempat@gmail.com>
2026-06-24 13:35:41 +0700

    fix(Tahap4): proxy /ws -> /websocket

--- vite.config.js
+++ vite.config.js
@@ -31,7 +31,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': { target: 'http://localhost:8787', changeOrigin: true },
-      '/ws': { target: 'ws://localhost:8787', ws: true },
+      '/websocket': { target: 'ws://localhost:8787', ws: true, changeOrigin: true },
     },
   },
 });
